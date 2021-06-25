728cd493812694ff107051a9ff2685f740106888
unknown <edgar.adirga@gmail.com>
2021-06-25 15:13:55 +0700

    more heroku stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV == development) require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 const cors = require('cors')
 const errorHandler = require('./middlewares/error-handler')
--- config/config.js
+++ config/config.js
@@ -7,18 +7,7 @@ module.exports = {
     "host": process.env.DEV_SERVER,
     "dialect": "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "config.use_env_variable": "DATABASE_URL"
   }
 }
